adcb596c574cad59bbee9fbe79238b5e1c030a33
jawedm89 <jawedm89@gmail.com>
2022-07-25 20:13:54 +0200

    a

---
+++ NodeWalk.js
@@ -0,0 +1,79 @@
+async function d(node, end, auslassen) {
+    let nexti = [];
+    let waiting = [];
+    if (node != null && node.start < end) {
+      if (Object.keys(node) != null && Object.keys(node)[0] === '0') {
+        for (let bodyy = 0; bodyy < Object.keys(node).length; bodyy++) {
+          if (bodyy === 0) {
+            nexti.push(node[bodyy]);
+          }
+          else {
+            waiting.unshift(node[bodyy])
+          }
+        }
+        return [nexti, waiting];
+      }
+      if (node.type === auslassen) {
+        return [nexti, waiting];
+      }
+      else {
+        let gefunden = false;
+        Object.entries(node).forEach(([key, value]) => {
+          if (typeof value === "object" && value != null) {
+            try {
+              value.forEach(element => {
+                if (element.type != auslassen && element.start < end) {
+                  if (gefunden === false) {
+                    nexti.push(element);
+                    gefunden = true;
+                  }
+                  else {
+                    waiting.unshift(element)
+                  }
+                }
+              })
+            }
+            catch (e) {
+              if (gefunden === false) {
+                nexti.push(value);
+                gefunden = true;
+              }
+              else {
+                waiting.unshift(value)
+              }
+            }
+          }
+  
+        });
+        return [nexti, waiting];
+      }
+    }
+    return [nexti, waiting];
+  }
+
+  async function NodeWalk(node, end, auslassen) {
+    let next = [node];
+    let arr = [];
+    let waiting = [];
+    if (auslassen === undefined) {
+      auslassen = "";
+    }
+    if (end === undefined) {
+      end = Number.POSITIVE_INFINITY;
+    }
+    do {
+      if (next.length > 0) {
+        arr.push(next[0]);
+        let result = await d(next[0], end, auslassen);
+        next = result[0];
+        result[1].forEach(element => { waiting.push(element) })
+      }
+      else {
+        arr.push(waiting[waiting.length - 1]);
+        let result = await d(waiting.pop(), end, auslassen);
+        next = result[0];
+        result[1].forEach(element => { waiting.push(element) });
+      }
+    } while (next.length > 0 || waiting.length > 0)
+    return arr;
+  }
--- bundle2.js
+++ bundle2.js
@@ -1,3 +1,4 @@
+//import { NodeWalk } from './NodeWalk.js';
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
   
   document.addEventListener("DOMContentLoaded", function(event) {
@@ -347,6 +348,7 @@
         }
       i = i -1;
     } while ( i > 0)
+    console.log("test hier" , found);
     if(SwitchOrIf.length > 0) {
       console.log(SwitchOrIf , found)
       for (let i = 0; found.length > i; i++) {
@@ -376,7 +378,8 @@
           //Suche alle Function calls, für jeden checken ob in or outside einer Funktion. 
         }
         else {
-          //such nach dem globalen wert
+          let searchHere = await NodeWalk(WebCryptoAPIScripts.ast, WebCryptoAPIScripts.ast.end, "FunctionDeclaration");
+
         }
       }
       else {
@@ -406,7 +409,7 @@
         return "OutSideFunction";
     }
   }
-
+/*
   async function d(node, end, auslassen) {
     let nexti = [];
     let waiting = [];
@@ -487,7 +490,7 @@
     return arr;
   }
 
-   
+  */ 
 });
 },{"acorn":3,"acorn-walk":2}],2:[function(require,module,exports){
 (function (global, factory) {
