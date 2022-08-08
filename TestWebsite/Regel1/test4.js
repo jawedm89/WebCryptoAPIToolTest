f0cbaf76e916f453172abbc021c536d9d6952c08
jawedm89 <jawedm89@gmail.com>
2022-08-08 15:18:21 +0200

    a

--- TestWebsite/Regel1/test4.js
+++ TestWebsite/Regel1/test4.js
@@ -22,3 +22,29 @@ async function enc() {
         ["encrypt", "decrypt"]);
     console.log(encryptCBC(key, window.crypto.getRandomValues(new Uint8Array(16))));
 }
+
+let encryptCBC2 = async function (key, rand) {
+    let chiffre = await window.crypto.subtle.encrypt(
+        {
+            name: "AES-CBC",
+            iv: rand,
+        },
+        key,
+        data
+    );
+    return chiffre;
+}
+
+let enc2 = async function () {
+    let iv =  window.crypto.getRandomValues(new Uint8Array(16));
+    let key = await window.crypto.subtle.generateKey({
+        name: "AES-CBC",
+        length: 256,
+    },
+        false,
+        ["encrypt", "decrypt"]);
+    return encryptCBC2(key, iv);
+}
+
+enc();
+enc2();
