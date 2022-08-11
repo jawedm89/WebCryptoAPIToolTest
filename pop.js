16eb47060399d514d8a79a14ebe41019953e5a7b
jawedm89 <jawedm89@gmail.com>
2022-08-11 14:51:32 +0200

    a

--- pop.js
+++ pop.js
@@ -21,8 +21,25 @@ browser.runtime.onMessage.addListener((request) => {
         request.Ergebnis.forEach(element => {
             let divi = document.getElementById("divi");
             let neu = document.createElement("div");
+            let button = document.createElement("button");
+            var text = document.createElement("textarea");
+            text.cols = "70";
+            text.rows = "5";
+            text.wrap = "off";
+            text.disabled = true;
+            button.textContent = "Script anzeigen";
+            button.addEventListener('click', function() {
+                text.className = "test";
+                /* 
+                element.regel1.forEach(elem => {
+                    text.setSelectionRange(0, 30)
+                }); */
+                text.value = (element.script);
+                neu.appendChild(text);
+            });
             neu.textContent = "es gab im folgenden Script ein API Call: " + element.src + ", an den stellen: " + element.regel1.join(', ')
             divi.appendChild(neu);
+            divi.appendChild(button);
         });
     }
 });
