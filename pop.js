6d3657a2b7436d70bac6e365a355ac955bfa7919
jawedm89 <jawedm89@gmail.com>
2022-08-11 17:14:18 +0200

    a

--- pop.js
+++ pop.js
@@ -15,31 +15,40 @@ a.addEventListener("click", function () {
         active: true
     }).then(sendMessageToTabs);
 });
+let clicked = false;
 
 browser.runtime.onMessage.addListener((request) => {
     if (request.Ergebnis) {
         request.Ergebnis.forEach(element => {
             let divi = document.getElementById("divi");
             let neu = document.createElement("div");
+            let neu2 = document.createElement("div");
             let button = document.createElement("button");
             var text = document.createElement("textarea");
             text.cols = "70";
             text.rows = "5";
             text.wrap = "off";
-            text.disabled = true;
+            text.className = "test";
+            text.value = (element.script);
             button.textContent = "Script anzeigen";
-            button.addEventListener('click', function() {
-                text.className = "test";
-                /* 
-                element.regel1.forEach(elem => {
-                    text.setSelectionRange(0, 30)
-                }); */
-                text.value = (element.script);
-                neu.appendChild(text);
-            });
             neu.textContent = "es gab im folgenden Script ein API Call: " + element.src + ", an den stellen: " + element.regel1.join(', ')
             divi.appendChild(neu);
             divi.appendChild(button);
+            neu.appendChild(neu2);
+            button.addEventListener('click', function () {
+                neu2.appendChild(text);
+                button.disabled = true;
+                let ar = [];
+                element.regel1.forEach(elem => {
+                    ar.push([elem, elem + 28])
+                });
+                $('.test').highlightWithinTextarea({
+                    highlight: ar
+                });
+                //$('.test').highlightWithinTextarea('update');
+            });
         });
+        //$('.test').highlightWithinTextarea('update');
+        //text.disabled = true;
     }
 });
