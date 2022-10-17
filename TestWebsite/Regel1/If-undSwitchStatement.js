0f19a8680aca1cf30dec80cb175cd6918ae9ece6
jawedm89 <jawedm89@gmail.com>
2022-10-18 00:07:58 +0200

    a

--- TestWebsite/Regel1/If-undSwitchStatement.js
+++ TestWebsite/Regel1/If-undSwitchStatement.js
@@ -12,7 +12,45 @@ async function func1R1T4() {
         iv = iv;
     }
     else {
-        iv = iv
+        iv = iv;
     }
     console.log(await window.crypto.subtle.encrypt({name: "AES-CBC",iv: iv}, await AESKeyR1T4, klarTextR1T4));
+}
+
+async function func2R1T4() {
+    let bool = true;
+    let iv;
+    if(bool) {
+        iv = window.crypto.getRandomValues(new Uint8Array(16));
+    }
+    else {
+        iv = new Uint8Array(16);
+    }
+    console.log(await window.crypto.subtle.encrypt({name: "AES-CBC",iv: iv}, await AESKeyR1T4, klarTextR1T4));
+}
+
+async function func3R1T4() {
+    let bool = true;
+    let iv2 = window.crypto.getRandomValues(new Uint8Array(16));
+    let iv;
+    if(bool) {
+        iv = new Uint8Array(16);
+    }
+    else {
+        iv = new Uint8Array(16);
+    }
+    console.log(await window.crypto.subtle.encrypt({name: "AES-CBC",iv: iv2}, await AESKeyR1T4, klarTextR1T4));
+}
+
+async function func4R1T4() {
+    let bool = true;
+    let iv2 = window.crypto.getRandomValues(new Uint8Array(16));
+    let iv;
+    if(bool) {
+        iv = new Uint8Array(16);
+    }
+    else {
+        iv = new Uint8Array(16);
+    }
+    console.log(await window.crypto.subtle.encrypt({name: "AES-CBC",iv: iv2}, await AESKeyR1T4, klarTextR1T4));
 }
